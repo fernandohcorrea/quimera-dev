e3f442ef16d70e9cedb72ecf810b78ffa3c438b7
Fernando H Corrêa <fernandohcorrea@gmail.com>
2024-08-20 16:32:39 -0300

    2024-08-20 - Build GitSubModules

--- ttools/Commands/InstallCommand.ts
+++ ttools/Commands/InstallCommand.ts
@@ -35,6 +35,7 @@ export default class InstallCommand extends AbstractCommand {
     await this.#getSubModules();
     await this.#doInstallGitSubmodules();
     await this.#doInstallDependencies();
+    await this.#doBuild();
     await this.#doCopyEnv();
     console.timeEnd("Install Time");
   }
@@ -253,6 +254,134 @@ export default class InstallCommand extends AbstractCommand {
 
           case "yarn":
             cmds.push(`yarn --cwd ${path_submodule} ${extra_install_args}`);
+            break;
+
+          default:
+            break;
+        }
+
+        cmds.push(`cd ${process.env.ROOT_PATH}`);
+
+        await this.shellCmdResult(cmds.join(" && "), true);
+      }
+    }
+  }
+
+  /**
+   * Do Build dos SubModules
+   */
+  async #doBuild() {
+    this.strOut.outSession(`Build SubModules`, "info");
+
+    let pkg_cfgs = this.config.get("packageManager");
+    pkg_cfgs = Object.assign(
+      {
+        submodules: {},
+      },
+      pkg_cfgs
+    );
+
+    let git_cfgs = this.config.get("git");
+    git_cfgs = Object.assign(
+      {
+        submodules: {},
+      },
+      git_cfgs
+    );
+
+    const git_default_cfg = Object.assign(
+      {
+        branch: "main",
+      },
+      git_cfgs?.default
+    );
+
+    const pkg_default_cfg = Object.assign(
+      {
+        exec: "npm",
+        install_args: [],
+      },
+      pkg_cfgs?.default
+    );
+
+    for (const key in this.submodules) {
+      if (Object.prototype.hasOwnProperty.call(this.submodules, key)) {
+        const submodule = this.submodules[key];
+        const path_submodule = path.join(
+          `${process.env.ROOT_PATH}`,
+          `/${submodule}`
+        );
+
+        if (!fs.existsSync(path_submodule)) {
+          this.strOut.outDanger(`Path ${path_submodule} não existe`);
+          continue;
+        }
+
+        let git_submodule =
+          Object.prototype.hasOwnProperty.call(
+            git_cfgs.submodules,
+            submodule
+          ) == true
+            ? git_cfgs.submodules[submodule]
+            : {};
+        git_submodule = Object.assign({}, git_default_cfg, git_submodule);
+
+        let pkg_submodule =
+          Object.prototype.hasOwnProperty.call(
+            pkg_cfgs.submodules,
+            submodule
+          ) == true
+            ? pkg_cfgs.submodules[submodule]
+            : {};
+        pkg_submodule = Object.assign({}, pkg_default_cfg, pkg_submodule);
+
+
+        const npm_submodule_path = path.join(
+          path_submodule,
+          "/package-lock.json"
+        );
+
+        const yarn_submodule_path = path.join(path_submodule, "/yarn.lock");
+
+        let package_manager_type: string | null = null;
+
+        switch (true) {
+          case fs.existsSync(yarn_submodule_path):
+            package_manager_type = "yarn";
+            break;
+
+          case fs.existsSync(npm_submodule_path):
+            package_manager_type = "npm";
+            break;
+
+          default:
+            this.strOut.outWarning(
+              `Path ${path_submodule} não é node(npm|yarn)`
+            );
+            continue;
+        }
+
+        this.strOut.outSession(
+          `== SubMolule - GIT: ${submodule.toUpperCase()} ==`,
+          "success"
+        );
+
+        let cmds = [`cd ${path_submodule}`];
+
+        const extra_build_args =
+          typeof pkg_submodule?.build_args == "object"
+            ? pkg_submodule.install_args.join(" ")
+            : "";
+
+        switch (package_manager_type) {
+          case "npm":
+            cmds.push(
+              `npm --prefix ${path_submodule} ${extra_build_args} run build `
+            );
+            break;
+
+          case "yarn":
+            cmds.push(`yarn --cwd ${path_submodule} ${extra_build_args} build`);
             break;
 
           default:
